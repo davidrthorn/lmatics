84df449270fbcc5bd7547ce8462759e77ba2b28d
David Rowthorn <davidrowthorn@gmail.com>
2020-05-16 15:00:37 +0100

    enable async await

--- client/src/components/app/index.jsx
+++ client/src/components/app/index.jsx
@@ -1,12 +1,35 @@
 import Chart from '../chart'
 import Input from '~/components/input'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const validateYear = year => /[1-9][0-9]{3}/.test(year) ? [true, ''] : [false, 'Not a valid year']
 const validateDisease = disease => [true, ''] // TODO: do we want validation?
 
 const App = () => {
   const [data, setData] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [isError, setIsError] = useState(false)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsError(false)
+      setIsLoading(true)
+
+      try {
+        const result = await fetch('http://localhost:3000/search?testparam=hello') // TODO: replace this placeholder
+        const body = await result.json()
+        setData(body.data)
+      } catch (err) {
+        setIsError(true)
+      }
+
+      setIsLoading(false)
+    }
+
+    fetchData()
+  }, [])
+
+  console.log(isError, isLoading) // TODO: remove this
 
   return (
     <div width='100vw' height='100vh'>
