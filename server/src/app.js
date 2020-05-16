98e141a45de977ddbbe3732d328647efce97471a
David Rowthorn <davidrowthorn@gmail.com>
2020-05-16 11:37:40 +0100

    placeholder search handler

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,14 @@
-import Express from 'express'
+const express = require('express')
 
-const app = Express()
+const app = express()
 const port = 3000 // TODO: load from environment
 const host = 'http://localhost' // TODO: load from enviroment
 
 app.get('/health', (req, res) => res.send('Healthy'))
+app.get('/search', searchHandler)
 
 app.listen(port, () => console.log(`Example app listening at ${host}:${port}`))
+
+function searchHandler (req, res) {
+  // TODO: implement the search
+}
