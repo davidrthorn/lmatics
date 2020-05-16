d0bb084f2cf63575d077022be06f4a0fd8a8c341
David Rowthorn <davidrowthorn@gmail.com>
2020-05-16 15:00:52 +0100

    enable cors from localhost

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,13 @@
 const express = require('express')
+const cors = require('cors')
 
 const app = express()
 const port = 3000 // TODO: load from environment
 const host = 'http://localhost' // TODO: load from enviroment
+
+app.use(cors({
+  origin: 'http://localhost:1234' // TODO: change this to read from ENV
+}))
 
 app.get('/health', (req, res) => res.send('Healthy'))
 app.get('/search', searchHandler)
@@ -10,5 +15,14 @@ app.get('/search', searchHandler)
 app.listen(port, () => console.log(`Example app listening at ${host}:${port}`))
 
 function searchHandler (req, res) {
-  // TODO: implement the search
+  res.json([
+    {
+      year: 1990,
+      count: 200
+    },
+    {
+      year: 1991,
+      count: 300
+    }
+  ])
 }
